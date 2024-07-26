23d58a23f085b0ea24134620f01e7beb905f75e1
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2024-07-26 15:27:03 +0200

    Finished Home component
    finished design of home component and made it completely dynamic with json provided file

---
+++ kasa/src/components/Home/Hero/hero.jsx
@@ -0,0 +1,11 @@
+import './style.sass';
+
+function Hero () {
+  return (
+    <div className="hero">
+    <h1 className="hero-title">Chez vous, <span className='mobile-break'>partout et ailleurs</span></h1>
+    </div>
+  );
+}
+
+export default Hero;
--- kasa/src/components/Home/home.jsx
+++ kasa/src/components/Home/home.jsx
@@ -1,11 +1,34 @@
-import Header from '../Header/header';
+import Hero from './Hero/hero';
+import './style.sass';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
+    const [logements, setLogements] = useState([]);
+
+    useEffect(() => {
+        fetch('/logements.json')
+        .then(response => response.json())
+        .then(data => setLogements(data));
+    }, []);
+
     return (
-        <>
-           
-        </>
+        <div className='home'>
+            <Hero />
+            <div className="home-content-container">
+                {logements.map((logement) => (
+                    <div key={logement.id} className='home-content'>
+                        <Link to={`/logement/${logement.id}`} className='home-link'>
+                            <div className='image-container'>
+                                <img src={logement.cover} alt={logement.title} className='home-cover' />
+                                <p className='home-title'>{logement.title}</p>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
-    }
+}
 
 export default Home;
