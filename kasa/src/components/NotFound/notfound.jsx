368b1ec3f93f0f66d557af58d3e343e625674cf2
Tom Hubert <tomhubert@Ordinateur-portable-de-Tom.local>
2024-07-27 01:40:58 +0200

    finished NotFound component

--- kasa/src/components/NotFound/notfound.jsx
+++ kasa/src/components/NotFound/notfound.jsx
@@ -1,10 +1,13 @@
 import './style.sass';
+import { Link } from 'react-router-dom';
+
 
 function NotFound () {
   return (
     <div className="notfound">
       <h1>404</h1>
-      <p>Not Found</p>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d’accueil</Link>
     </div>
   )
 }
